3bbf492b9af244467a1cd6f00d3801c9fea118ac
Quinton <quinton.xu@gmail.com>
2016-12-12 14:49:35 +0800

    fix

--- test/index.js
+++ test/index.js
@@ -3,18 +3,22 @@ const amigo = require('../lib/index');
 
 describe('Oauth With Amigo', function() {
     const context = {
-        host : 't-id.amigo.cn', //测试环境
-        port : '6443',
-        timestamp : new Date().getTime(), //时间戳,距离 1970-01-01 00:00:00 GMT 的秒数。
-        nonce:'1qazxcvgt54', // 随机字符串。
+        host : 'id.amigo.cn',
+        port : '443',
+        timestamp : Math.floor(Date.now() / 1000), //时间戳,距离 1970-01-01 00:00:00 GMT 的秒数。
+        //2rT3gh72u8GbQARNn556eEZgmpo=
+        nonce:'8FINtYTUsKbSZGfl', // 随机字符串。
         method:'POST',
         uri: '/account/verify.do',
-        appId:'A05B5DA7546645F99840F1B5578707D2' //应用程序标识,由帐号系统分配
+        appId:'', //应用程序标识,由帐号系统分配, appId
+        appKey:'' //应用程序标识,由帐号系统分配, appKey
     };
-    const token = {"h":"BBFA0DB5590D430399AA6A912191E37B","n":"2494BABE","t":"1481250344","v":"EEF0C3AAF3636628B333B77A10543D6169D2D333"};
+    //替换有效的token
+    const token = {'h':'9DEDD8D5E6F04C2AA6EB34DED0721315','n':'BCC6D4CE','t':'1481523626','v':'CC0A4AE0E3CD30868F107D7CF0026446A8401721'};
     it('should return {"wid": "xxx","r": "xxx","err": "不存在该应用(AppId|AppKey)"} when the value is not present', function(done) {
         amigo.macSign(token, context, function (err, res)  {
             done();
+            console.log(res);
             assert.equal(res.err, "不存在该应用(AppId|AppKey)");
             assert.equal(res.r, "1011");
         });
